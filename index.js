b55211f64bee56bae18eff965d4f562bfc230cc6
Dmitrii <chernishow.dima@yandex.ru>
2021-04-06 23:29:01 +0300

    Выделение вычислений

--- index.js
+++ index.js
@@ -2,81 +2,107 @@ var operator = document.getElementsByClassName("operator");
 var number = document.getElementsByClassName("number");
 
 function getFormattedNumber(num) {
-    if (num == "-") {
-        return "";
-    }
-    return Number(num).toLocaleString("en");
+    return num == "-" ? "" : Number(num).toLocaleString("en");
 }
 
 function reverseNumberFormat(num) {
     return Number(num.replace(/,/g, ''));
 }
 
-function main() {
-    for (var i = 0; i < operator.length; i++) {
-        operator[i].addEventListener('click', function () {
-            if (this.id == "clear") {
-                document.getElementById("history-value").innerText = "";
-                document.getElementById("output-value").innerText = "";
-            }
-            else if (this.id == "backspace") {
-                var output = reverseNumberFormat(document.getElementById("output-value").innerText).toString();
-                if (output) {
-                    output = output.substr(0, output.length - 1);
-                    if (output == "") {
-                        document.getElementById("output-value").innerText = output;
-                    }
-                    else {
-                        document.getElementById("output-value").innerText = getFormattedNumber(output);
-                    }
-                }
-            }
-            else {
-                var output = document.getElementById("output-value").innerText;
-                var history = document.getElementById("history-value").innerText;
-                if (output == "" && history != "") {
-                    if (isNaN(history[history.length - 1])) {
-                        history = history.substr(0, history.length - 1);
-                    }
-                }
-                if (output != "" || history != "") {
-                    output = output == "" ? output : reverseNumberFormat(output);
-                    history = history + output;
-                    if (this.id == "=") {
-                        var result = eval(history);
-                        if (result == "") {
-                            document.getElementById("output-value").innerText = result;
-                        }
-                        else {
-                            document.getElementById("output-value").innerText = getFormattedNumber(result);
-                        }
-                        document.getElementById("history-value").innerText = "";
-                    }
-                    else {
-                        history = history + this.id;
-                        document.getElementById("history-value").innerText = history;
-                        document.getElementById("output-value").innerText = "";
-                    }
-                }
-            }
-    
-        });
-    }
-    
-    for (var i = 0; i < number.length; i++) {
-        number[i].addEventListener('click', function () {
-            var output = reverseNumberFormat(document.getElementById("output-value").innerText);
-            if (output != NaN) {
-                output = output + this.id;
-                if (output == "") {
-                    document.getElementById("output-value").innerText = output;
-                }
-                else {
-                    document.getElementById("output-value").innerText = getFormattedNumber(output);
-                }
-            }
-        });
+function updateHistory(newValue) {
+    document.getElementById("history-value").innerText = newValue;
+}
+
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+}
+
+function updateOutput(newValue) {
+    document.getElementById("output-value").innerText = newValue;
+}
+
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+function clearWorker() {
+    updateHistory("");
+    updateOutput("");
+}
+
+function backspaceWorker() {
+    var output = reverseNumberFormat(document.getElementById("output-value").innerText).toString();
+    if (output) {
+        output = output.substr(0, output.length - 1);
+        if (output == "") {
+            updateOutput(output);
+        }
+        else {
+            updateOutput(getFormattedNumber(output));
+        }
     }
 }
 
-main();
+function removeLastNaN(string) {
+    var result = string;
+    if (isNaN(result[result.length - 1])) {
+        result = result.substr(0, result.length - 1);
+    }
+    return result;
+}
+
+function operatorWorker(el) {
+    var output = getOutput();
+    var history = getHistory();
+    if (output == "" && history != "") { // Если пустой вывод и нечего считать
+        history = removeLastNaN(history);
+    }
+    if (output != "" || history != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        history += output;
+        if (el.id == "=") {
+            var result = eval(history);
+            if (result == "") {
+                updateOutput(result);
+            }
+            else {
+                updateOutput(getFormattedNumber(result));
+            }
+            updateHistory("");
+        }
+        else {
+            history += el.id;
+            updateHistory(history);
+            updateOutput("");
+        }
+    }
+}
+
+function numberClickWorker(el) {
+    var output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+        output += el.id;
+        if (output == "") {
+            updateOutput(output);
+        }
+        else {
+            updateOutput(getFormattedNumber(output));
+        }
+    }
+}
+
+function main(operators, numbers) {
+    for (var i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', function () {
+            if (this.id == "clear") { clearWorker(); }
+            else if (this.id == "backspace") { backspaceWorker(); }
+            else { operatorWorker(this); }
+        });
+    }
+
+    for (var i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', function () { numberClickWorker(this); });
+    }
+}
+
+main(operator, number);
